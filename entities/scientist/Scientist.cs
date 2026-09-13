ac7013458e7c8301a31fbc10f4956e5e1e8ee75d
Ethan Baedke <ethanbaedke@gmail.com>
2026-09-13 14:09:28 -0500

    Improved scientist movement.

--- entities/scientist/Scientist.cs
+++ entities/scientist/Scientist.cs
@@ -3,21 +3,34 @@ using System;
 
 public partial class Scientist : CharacterBody2D
 {
+	const double MAX_H_SPEED = 1000.0f;
+	const double H_ACCELERATION = 10000.0f;
+	const double MAX_V_SPEED = 2000.0f;
+	const double V_ACCELERATION = 10000.0f;
+	const double JUMP_FORCE = 2500.0f;
+
+	private float _movement_direction = 0.0f;
+
 	// Expects a direciton between (-1.0, 1.0). Should be called every frame during movement.
 	public void SetMovementDirection(float dir)
 	{
-		this.Velocity = new Vector2(100.0f * dir, this.Velocity.Y);
+		_movement_direction = dir;
 	}
 
 	// Should be called once to trigger a jump.
 	public void Jump()
 	{
-		this.Velocity = new Vector2(this.Velocity.X, -10000.0f);
+		this.Velocity = new Vector2(this.Velocity.X, -(float)JUMP_FORCE);
 	}
 
 	public override void _PhysicsProcess(double delta)
 	{
+		// Apply gravity.
+		this.Velocity = this.Velocity.MoveToward(new Vector2(this.Velocity.X, (float)MAX_V_SPEED), (float)(V_ACCELERATION * delta));
+
+		// Apply movement direction.
+		this.Velocity = this.Velocity.MoveToward(new Vector2((float)MAX_H_SPEED * _movement_direction, this.Velocity.Y), (float)(H_ACCELERATION * delta));
+
 		MoveAndSlide();
-		this.Velocity = new Vector2(0.0f, 200.0f);
 	}
 }
