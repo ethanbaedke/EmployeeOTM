14722593aa305763befe588bbb449e71edb84232
Ethan Baedke <110857520+ethanbaedke@users.noreply.github.com>
2026-09-19 17:59:18 -0500

    Implemented capturing capture areas.

--- entities/CaptureArea.cs
+++ entities/CaptureArea.cs
@@ -6,8 +6,74 @@ public partial class CaptureArea : Area2D
 {
     [Export] private Sprite2D _sprite;
 
+    private const double TIME_TO_CAPTURE = 2.0f;
+
     private ShaderMaterial _shaderMat;
     private Array<Scientist> _scientistsInArea = new Array<Scientist>();
+    private Scientist _controllingScientist = null;
+    private Scientist _capturingScientist = null;
+    private Color _uncontrolledColor = Colors.Gray;
+    private double _capturePercent = 0.0f;
+
+    private void UpdateCaptureProgress(double delta)
+    {
+        // A single scientist who doesn't own the area is present.
+        if (_scientistsInArea.Count == 1 && _scientistsInArea[0] != _controllingScientist)
+        {
+            // The scientist just moved in. Resetting progress and beginning capture.
+            if (_scientistsInArea[0] != _capturingScientist)
+            {
+                OTMLogger.Instance.Info(this, "Beginning capture.");
+                _capturingScientist = _scientistsInArea[0];
+                _capturePercent = 0.0;
+            }
+
+            // Progress the capture.
+            _capturePercent += delta / TIME_TO_CAPTURE;
+
+            // Successful capture.
+            if (_capturePercent >= 1.0)
+            {
+                OTMLogger.Instance.Info(this, "Area captured.");
+                _controllingScientist = _capturingScientist;
+                _capturingScientist = null;
+                _capturePercent = 0.0;
+            }
+        }
+        // The capturing scientist has left the area.
+        else if (_capturingScientist != null && _scientistsInArea.Contains(_capturingScientist) == false)
+        {
+            OTMLogger.Instance.Info(this, "Resetting capture progress.");
+            _capturingScientist = null;
+            _capturePercent = 0.0;
+        }
+        // No one is capturing the area.
+        else if (_capturingScientist == null)
+        {
+            _capturePercent = 0.0;
+        }
+
+        _shaderMat.SetShaderParameter("CapturePercent", _capturePercent);
+    }
+
+    private void UpdateColors()
+    {
+        // Set the background color to the color of whoever is controlling the area, and gray if it's uncontrolled.
+        if (_controllingScientist != null)
+        {
+            _shaderMat.SetShaderParameter("OwnerColor", _controllingScientist.ScientistColor);
+        }
+        else
+        {
+            _shaderMat.SetShaderParameter("OwnerColor", _uncontrolledColor);
+        }
+
+        // Set the capture progress color to whoever is capture the area.
+        if (_capturingScientist != null)
+        {
+            _shaderMat.SetShaderParameter("ContesterColor", _capturingScientist.ScientistColor);
+        }
+    }
 
     private void OnBodyEntered(Node2D body)
     {
@@ -36,9 +102,18 @@ public partial class CaptureArea : Area2D
         base._Ready();
 
         // Grab a reference to the shader material on our sprite.
-        // TODO
+        _shaderMat = _sprite.Material as ShaderMaterial;
 
         this.BodyEntered += OnBodyEntered;
         this.BodyExited += OnBodyExited;
     }
+
+    public override void _Process(double delta)
+    {
+        base._Process(delta);
+
+        UpdateCaptureProgress(delta);
+        UpdateColors();
+    }
+
 }
