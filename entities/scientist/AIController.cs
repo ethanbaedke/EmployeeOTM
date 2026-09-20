b71e9b6cdb6c422758a93145304e8e76c20ed47b
Zach Overman <zvoverman@gmail.com>
2026-09-20 11:45:22 -0600

    format project

--- entities/scientist/AIController.cs
+++ entities/scientist/AIController.cs
@@ -3,45 +3,45 @@ using System;
 
 public partial class AIController : ScientistController
 {
-	const double DECISION_COOLDOWN = 1.0f;
-	private double _decisionTimer = 0.0f;
+    const double DECISION_COOLDOWN = 1.0f;
+    private double _decisionTimer = 0.0f;
 
-	private float _moveDir = 0.0f;
+    private float _moveDir = 0.0f;
 
-	private void MakeDecision()
-	{
-		Scientist scientist = TryGetScientist();
-		if (scientist == null) return;
+    private void MakeDecision()
+    {
+        Scientist scientist = TryGetScientist();
+        if (scientist == null) return;
 
-		int decision = GD.RandRange(0, 1);
-		if (decision == 0)
-		{
-			_moveDir = (float)GD.RandRange(-1.0, 1.0);
-		}
-		else
-		{
-			scientist.Jump();
-		}
-	}
+        int decision = GD.RandRange(0, 1);
+        if (decision == 0)
+        {
+            _moveDir = (float)GD.RandRange(-1.0, 1.0);
+        }
+        else
+        {
+            scientist.Jump();
+        }
+    }
 
     public override void _Process(double delta)
     {
         base._Process(delta);
 
-		Scientist scientist = TryGetScientist();
-		if (scientist == null) return;
+        Scientist scientist = TryGetScientist();
+        if (scientist == null) return;
 
-		scientist.SetMovementDirection(_moveDir);
+        scientist.SetMovementDirection(_moveDir);
 
-		if (_decisionTimer > 0.0f)
-		{
-			_decisionTimer -= delta;
-			return;
-		}
-		else
-		{
-			MakeDecision();
-			_decisionTimer = DECISION_COOLDOWN;
-		}
+        if (_decisionTimer > 0.0f)
+        {
+            _decisionTimer -= delta;
+            return;
+        }
+        else
+        {
+            MakeDecision();
+            _decisionTimer = DECISION_COOLDOWN;
+        }
     }
 }
--- entities/scientist/PlayerController.cs
+++ entities/scientist/PlayerController.cs
@@ -5,83 +5,83 @@ using System.Linq;
 
 public partial class PlayerController : ScientistController
 {
-	public int InputDevice = -2;
+    public int InputDevice = -2;
 
-	private Dictionary<Settings.InputAction, bool> actionStates = new Dictionary<Settings.InputAction, bool>();
+    private Dictionary<Settings.InputAction, bool> actionStates = new Dictionary<Settings.InputAction, bool>();
 
     public override void _Process(double delta)
     {
         base._Process(delta);
 
-		Scientist scientist = TryGetScientist();
-		if (scientist == null) return;
+        Scientist scientist = TryGetScientist();
+        if (scientist == null) return;
 
-		// If on keyboard, use left and right move actions for movement.
-		if (InputDevice == -1)
-		{
-			float moveDir = 0.0f;
-			if (actionStates.ContainsKey(Settings.InputAction.MOVE_LEFT) && actionStates[Settings.InputAction.MOVE_LEFT])
-			{
-				moveDir -= 1.0f;
-			}
-			if (actionStates.ContainsKey(Settings.InputAction.MOVE_RIGHT) && actionStates[Settings.InputAction.MOVE_RIGHT])
-			{
-				moveDir += 1.0f;
-			}
+        // If on keyboard, use left and right move actions for movement.
+        if (InputDevice == -1)
+        {
+            float moveDir = 0.0f;
+            if (actionStates.ContainsKey(Settings.InputAction.MOVE_LEFT) && actionStates[Settings.InputAction.MOVE_LEFT])
+            {
+                moveDir -= 1.0f;
+            }
+            if (actionStates.ContainsKey(Settings.InputAction.MOVE_RIGHT) && actionStates[Settings.InputAction.MOVE_RIGHT])
+            {
+                moveDir += 1.0f;
+            }
 
-			scientist.SetMovementDirection(moveDir);
-		}
+            scientist.SetMovementDirection(moveDir);
+        }
     }
 
     public override void _UnhandledInput(InputEvent @event)
     {
         base._UnhandledInput(@event);
 
-		if (@event.IsEcho()) return;
+        if (@event.IsEcho()) return;
 
-		int deviceInd = @event.Device;
-		if (@event is InputEventKey)
-		{
-			deviceInd = -1;
-		}
-		if (deviceInd != InputDevice) return;
+        int deviceInd = @event.Device;
+        if (@event is InputEventKey)
+        {
+            deviceInd = -1;
+        }
+        if (deviceInd != InputDevice) return;
 
-		Scientist scientist = TryGetScientist();
-		if (scientist == null) return;
+        Scientist scientist = TryGetScientist();
+        if (scientist == null) return;
 
-		Settings.InputAction action = Settings.InputAction.NONE;
-		if (@event is InputEventKey keyEvent)
-		{
-			action = OTMSaveData.Instance.Settings.GetActionFromKey(keyEvent.Keycode);
-		}
-		else if (@event is InputEventJoypadButton buttonEvent)
-		{
-			action = OTMSaveData.Instance.Settings.GetActionFromButton(buttonEvent.ButtonIndex);
-		}
-		else if (@event is InputEventJoypadMotion motionEvent)
-		{
-			action = OTMSaveData.Instance.Settings.GetActionFromAxis(motionEvent.Axis);
-			if (action == Settings.InputAction.MOVE_AXIS)
-			{
-				scientist.SetMovementDirection(motionEvent.AxisValue);
-				GetViewport().SetInputAsHandled();
-				return;
-			}
-		}
+        Settings.InputAction action = Settings.InputAction.NONE;
+        if (@event is InputEventKey keyEvent)
+        {
+            action = OTMSaveData.Instance.Settings.GetActionFromKey(keyEvent.Keycode);
+        }
+        else if (@event is InputEventJoypadButton buttonEvent)
+        {
+            action = OTMSaveData.Instance.Settings.GetActionFromButton(buttonEvent.ButtonIndex);
+        }
+        else if (@event is InputEventJoypadMotion motionEvent)
+        {
+            action = OTMSaveData.Instance.Settings.GetActionFromAxis(motionEvent.Axis);
+            if (action == Settings.InputAction.MOVE_AXIS)
+            {
+                scientist.SetMovementDirection(motionEvent.AxisValue);
+                GetViewport().SetInputAsHandled();
+                return;
+            }
+        }
 
-		// If we've never seen this action before, add it as disabled.
-		if (actionStates.ContainsKey(action) == false)
-		{
-			actionStates[action] = false;
-		}
+        // If we've never seen this action before, add it as disabled.
+        if (actionStates.ContainsKey(action) == false)
+        {
+            actionStates[action] = false;
+        }
 
-		// Only listen to the first press for jumping.
-		if (action == Settings.InputAction.JUMP && actionStates[Settings.InputAction.JUMP] == false)
-		{
-			scientist.Jump();
-		}
+        // Only listen to the first press for jumping.
+        if (action == Settings.InputAction.JUMP && actionStates[Settings.InputAction.JUMP] == false)
+        {
+            scientist.Jump();
+        }
 
-		actionStates[action] = @event.IsPressed();
-		GetViewport().SetInputAsHandled();
+        actionStates[action] = @event.IsPressed();
+        GetViewport().SetInputAsHandled();
     }
 }
